dc2ecbe708e15230a5fb828b756a35c0e9006ffd
bokhanych <d.bakhanko@yandex.by>
2023-04-05 13:58:07 +0300

    modified

--- Terraform/main.tf
+++ Terraform/main.tf
@@ -9,7 +9,7 @@ resource "aws_instance" "app-server" {
     tags = { Name = "APP-SERVER" }
     vpc_security_group_ids = [aws_security_group.java.id]
     key_name        = data.aws_key_pair.selected.key_name
-    user_data = "${file("uploads/docker-install.sh")}"
+    user_data = "${file("uploads/app-server-config.sh")}"
     provisioner "file" {
       source      = "uploads/myapp.conf"
       destination = "CHANGE ME"
--- Terraform/uploads/jenkins-server-setup.sh
+++ Terraform/uploads/jenkins-server-setup.sh
@@ -1,4 +1,14 @@
 #!/bin/bash
+# Jenkins install for Ubuntu
+sudo apt update -y;
+sudo apt install default-jdk maven net-tools -y;
+echo "deb https://pkg.jenkins.io/debian-stable binary/" > jenkins.list;
+sudo mv jenkins.list /etc/apt/sources.list.d/;
+sudo wget -q -O - https://pkg.jenkins.io/debian-stable/jenkins.io.key | sudo apt-key add -;
+sudo mv /etc/apt/trusted.gpg /etc/apt/trusted.gpg.d/;
+sudo apt update -y;
+sudo apt install jenkins -y;
+sudo systemctl enable jenkins && sudo systemctl start jenkins
 # Docker install for Ubuntu
 sudo apt update -y;
 sudo apt-get install -y ca-certificates curl gnupg
@@ -8,3 +18,4 @@ echo "deb [arch="$(dpkg --print-architecture)" signed-by=/etc/apt/keyrings/docke
 sudo apt-get update -y;
 sudo apt-get -y install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
 sudo systemctl enable docker && sudo systemctl start docker
+sudo usermod -aG docker jenkins && sudo chmod 666 /var/run/docker.sock
