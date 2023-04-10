e94c6fff031b8ab84f1f1e7ec2b1a091ac20a206
bokhanych <d.bakhanko@yandex.by>
2023-04-10 16:06:47 +0300

    add grafana dashboard

--- scripts/docker-install.sh
+++ scripts/docker-install.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # Docker install for Ubuntu
 apt update -y;
-apt-get install -y ca-certificates curl gnupg net-tools
+apt-get install -y ca-certificates curl gnupg net-tools net-tools git
 mkdir -m 0755 -p /etc/apt/keyrings
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg |  gpg --dearmor -o /etc/apt/keyrings/docker.gpg
 echo "deb [arch="$(dpkg --print-architecture)" signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu "$(. /etc/os-release && echo "$VERSION_CODENAME")" stable" |  tee /etc/apt/sources.list.d/docker.list > /dev/null
--- scripts/monitoring-setup.sh
+++ scripts/monitoring-setup.sh
@@ -8,21 +8,21 @@ cat << EOF > /etc/docker/daemon.json
 EOF
 service docker restart
 
-mkdir /etc/monitoring/
-cat << EOF > /etc/monitoring/docker-compose.yml
+cat << EOF > /etc/docker-compose.yml
 version: '3.7'
 services:
     prometheus:
       image: prom/prometheus:latest
       container_name: prometheus
       volumes:
-      - ./prometheus/:/etc/monitoring/prometheus/
+      - ./prometheus/:/etc/prometheus/
       command:
-      - '--config.file=/etc/monitoring/prometheus/prometheus.yml'
-      - '--storage.tsdb.path=/prometheus'
+      - '--config.file=/etc/prometheus/prometheus.yml'
       ports:
       - '9090:9090'
       restart: unless-stopped
+      networks:
+      - default
     blackbox:
       image: prom/blackbox-exporter:latest
       container_name: blackbox
@@ -30,12 +30,14 @@ services:
       depends_on:
       - prometheus
       command:
-      - "--config.file=/etc/monitoring/blackbox/blackbox.yml"
+      - "--config.file=/etc/blackbox/blackbox.yml"
       volumes:
-      - ./blackbox/:/etc/monitoring/blackbox/
+      - ./blackbox/:/etc/blackbox/
       ports:
       - '9115:9115'
       restart: unless-stopped
+      networks:
+      - default
     grafana:
       image: grafana/grafana:latest
       container_name: grafana
@@ -47,15 +49,18 @@ services:
       - GF_SECURITY_ADMIN_USER=admin
       - GF_SECURITY_ADMIN_PASSWORD=grafana
       volumes:
-      - ./grafana:/etc/monitoring/grafana/provisioning/datasources
-volumes:
-  prom_data:
+      - ./grafana/datasources:/etc/grafana/provisioning/datasources
+      - ./grafana/dashboards:/etc/grafana/provisioning/dashboards
+      networks:
+      - default
 EOF
 
 # Grafana-datasources
-mkdir -p /etc/monitoring/grafana/provisioning/datasources
-cat << EOF > /etc/monitoring/grafana/provisioning/datasource.yml
+mkdir -p /etc/grafana/datasources
+touch /etc/grafana/datasources/datasource.yml
+cat << EOF > /etc/grafana/datasources/datasource.yml
 apiVersion: 1
+
 datasources:
 - name: Prometheus
   type: prometheus
@@ -65,34 +70,53 @@ datasources:
   editable: true
 EOF
 
+# Grafana-dashboards
+mkdir /etc/grafana/dashboards
+cat << EOF > /etc/grafana/dashboards/dashboard.yml
+apiVersion: 1
+
+providers:
+- name: 'a unique provider name'
+  orgId: 1
+  folder: ''
+  folderUid: ''
+  type: file
+  disableDeletion: false
+  editable: true
+  updateIntervalSeconds: 10
+  options:
+    path: /etc/grafana/provisioning/dashboards
+EOF
+cd /tmp && git clone https://github.com/bokhanych/ci-cd.git && cp ci-cd/grafana-dashboard/docker-url_by_bokhanych.json /etc/grafana/dashboards/ && rm -r ci-cd
+
 # Prometheus-config
-mkdir -p /etc/monitoring/prometheus
-cat << EOF > /etc/monitoring/prometheus/prometheus.yml
+mkdir /etc/prometheus
+cat << EOF > /etc/prometheus/prometheus.yml
 # Set global configuration
 global:
-  scrape_interval:     15s
-  evaluation_interval: 15s
+  scrape_interval:     5s
+  evaluation_interval: 5s
   external_labels:
       monitor: 'my-project'
 rule_files:
 scrape_configs:
-  - job_name: 'prometheus'
-    scrape_interval: 5s
-    static_configs:
-         - targets: ['prometheus:9090']
+
   - job_name: 'docker-metrics'
     scrape_interval: 5s
     static_configs:
       - targets: ['172.17.0.1:9323'] 
+
   - job_name: 'blackbox'
     metrics_path: /probe
     params:
       module: [http_2xx]
     static_configs:
       - targets:
+        - http://localhost:9090
+        - http://localhost:3000
         - http://localhost:8080/helloworld/hello
-        - http://prometheus:9090
-        - http://grafana:3000
+        - http://localhost:9115
+
     relabel_configs:
       - source_labels: [__address__]
         target_label: __param_target
@@ -101,13 +125,12 @@ scrape_configs:
       - target_label: __address__
         replacement: blackbox:9115
 EOF
-
 EXTERNAL_IP=$(hostname  -I | cut -f1 -d' ');
-sed -i "s%localhost%$EXTERNAL_IP%g" /etc/monitoring/prometheus/prometheus.yml;
+sed -i "s%localhost%$EXTERNAL_IP%g" /etc/prometheus/prometheus.yml;
 
 # Blackbox-config
-mkdir -p /etc/monitoring/blackbox
-cat << EOF > /etc/monitoring/blackbox/blackbox.yml
+mkdir /etc/blackbox
+cat << EOF > /etc/blackbox/blackbox.yml
 modules:
   http_2xx:
     http:
@@ -121,6 +144,15 @@ modules:
     timeout: 5s
 EOF
 
+#cd /etc/ && docker compose up -d
 
 
-cd /etc/monitoring && docker compose up -d
+
+# CHANGE:
+# cd ~ && vi mon.sh
+
+# RUN: 
+# cd ~ && bash mon.sh
+
+# CLEAR: 
+# cd /etc && docker kill $(docker ps -q) && docker system prune -a -f && rm -r /etc/grafana /etc/prometheus /etc/blackbox /etc/docker-compose.yml && rm /etc/docker/daemon.json
